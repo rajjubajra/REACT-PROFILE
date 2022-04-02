0f97ecf477403d4dcd583d9a6183953671e73d0c
rajjubajra <rajjubajra@gmail.com>
2022-04-02 13:10:02 +0100

    tw3_profile_10 re worked

--- tw3_profile_10/src/App.js
+++ tw3_profile_10/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Layout from './layout/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Explore from './pages/Explore';
+
 
 function App() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <h1>Page is under maintenance</h1>
-    </div>
+    <BrowserRouter>
+    <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+    </Layout>
+    </BrowserRouter>
   );
 }
 
